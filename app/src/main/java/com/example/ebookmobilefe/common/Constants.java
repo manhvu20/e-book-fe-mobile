d9c8d74d37573c9cfffe520b8dd6b2c5c2e067ec
HP\daohu <daohuyhieu1704@gmail.com>
2024-02-25 01:33:20 +0700

    HieuDh: config network

--- app/src/main/java/com/example/ebookmobilefe/common/Constants.java
+++ app/src/main/java/com/example/ebookmobilefe/common/Constants.java
@@ -2,5 +2,5 @@ package com.example.ebookmobilefe.common;
 
 public class Constants {
     private Constants() {}
-    public static final String BASE_URL = "http://localhost:8000";
+    public static final String BASE_URL = "http://10.0.2.2:8000";
 }
--- app/src/main/java/com/example/ebookmobilefe/model/LoginRequest.java
+++ app/src/main/java/com/example/ebookmobilefe/model/LoginRequest.java
@@ -1,16 +1,19 @@
 package com.example.ebookmobilefe.model;
 
 public class LoginRequest {
-    private String username;
+    private String email;
     private String password;
 
-    public LoginRequest(String username, String password) {
-        this.username = username;
+    public LoginRequest(String email, String password) {
+        this.email = email;
         this.password = password;
     }
-    public String getUsername() {
-        return username;
+
+    // Getters
+    public String getEmail() {
+        return email;
     }
+
     public String getPassword() {
         return password;
     }
