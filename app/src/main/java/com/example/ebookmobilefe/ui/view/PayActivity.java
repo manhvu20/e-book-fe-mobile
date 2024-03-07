db9d2148658f230b43ffcc12196437c6174cde7e
mynameisnhi <tuyetnhi260802@gmail.com>
2024-03-07 16:04:55 +0700

    layout listbook, cart, pay

---
+++ app/src/main/java/com/example/ebookmobilefe/ui/view/PayActivity.java
@@ -0,0 +1,16 @@
+package com.example.ebookmobilefe.ui.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ebookmobilefe.R;
+
+public class PayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
